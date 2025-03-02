b8077b7eb88af5c095f392a05cb16fef1b9dcb2c
Mehebul02 <mehebul2123@gmail.com>
2025-03-02 14:46:59 +0600

    added the font

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,9 +1,14 @@
+import Image from 'next/image';
 import React from 'react';
 
 const GalleryPage = () => {
     return (
-        <div>
+        <div className='flex flex-col items-center'>
            <h1>gallery page</h1>
+           {/* <img src = 'https://www.axelerant.com/hubfs/Imported_Blog_Media/nextjs_image1.jpg'alt='next jd'/> */}
+
+           <h1>Next Js components image</h1>
+           <Image src='https://www.axelerant.com/hubfs/Imported_Blog_Media/nextjs_image1.jpg' loading='lazy' alt='next image' height={400} width={400}/>
         </div>
     );
 };
