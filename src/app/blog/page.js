363aab05b728908263d039aa61234dbaa108f424
Mehebul02 <mehebul2123@gmail.com>
2025-03-03 05:42:54 +0600

    Added the data facing

---
+++ src/app/blog/page.js
@@ -0,0 +1,15 @@
+
+export const metadata = {
+    title:'Blog',
+    description:'Blog page'
+}
+
+const BlogPage = () => {
+    return (
+        <div>
+            <h1>Blog page</h1>
+        </div>
+    );
+};
+
+export default BlogPage;
